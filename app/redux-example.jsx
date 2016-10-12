79c236ef0c75ca5efb74dfb551f2ca16ca41c3d2
Arend Hosman <arend@pti.nl>
2016-10-12 16:16:01 +0200

    Add actions for adding/removing hobbies and movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,15 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
 
   switch (action.type) {
     case 'CHANGE_NAME':
@@ -10,8 +18,41 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
         ...state,
         name: action.name
       };
-      default:
-        return state;
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      }
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      }
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
+      }
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      }
+    default:
+      return state;
   }
 };
 var store = redux.createStore(reducer, redux.compose(
@@ -23,6 +64,8 @@ var unsubscribe = store.subscribe(() => {
   var state = store.getState();
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+
+  console.log('New state', store.getState());
 });
 // unsubscribe();
 
@@ -32,6 +75,44 @@ console.log('currentState ', currentState);
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Arend'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Running'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Jaws',
+  genre: 'Thriller'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Eternal sunshine of the spotless mind',
+  genre: 'Drama comedy'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Oh brother where art thou?',
+  genre: 'Comedy'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
 });
 
 store.dispatch({
